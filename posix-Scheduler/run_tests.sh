de035744d4688685e47c34627174310454fc6304
Jenna <jsteph11@calpoly.edu>
2020-01-23 14:59:31 -0800

    first test case working

--- posix-Scheduler/run_tests.sh
+++ posix-Scheduler/run_tests.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
+run_test () {
+	make clean &>/dev/null
+	make $1 &>/dev/null
+	OUT=./test_output/out_fcfs_$2
+	if (test -f $OUT) 
+		then 
+			DIFF=$(diff <(./$1 $2) $OUT)
+			if [ !"$DIFF" ]
+				then
+					printf "pass"
+				else
+					printf "FAILED"
+			fi
+		else
+			printf "No matching output file. Fail."
+		fi
+}
+
 printf "Running Test Cases..................\n"
 for file in *.txt
 do
-	echo "----------	Test Cases for $file ------------------"
+	printf "Test Cases for $file------------------\n"
 	printf "\nfcfs..."
-	make clean &>/dev/null
-	make fcfs &>/dev/null
-	DIFF=$(diff <(./fcfs $file) ./test_output/out_fcfs_$file)
-	if [ !"$DIFF" ]
-	then
-		printf "pass"
-	fi
+	run_test "fcfs" "$file"
+	
 	printf "\nsjf..."
 	printf "\nrr..."
 	printf "\npriority..."
